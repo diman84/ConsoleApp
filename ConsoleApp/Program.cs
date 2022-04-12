28aab444bf184fdaa91c16b8922f23349c4a946d
Dmitry <diman.84@gmail.com>
2022-04-12 21:30:37 +0300

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Dangl.Calculator;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет\n напиши формулу");
+            var formula = Console.ReadLine();
+
+            var calculation = Calculator.Calculate(formula);
+
+            Console.WriteLine(calculation.Result);
+            Console.WriteLine(calculation.IsValid);
+        }
+    }
+}
